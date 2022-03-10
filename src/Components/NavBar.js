c3346ba7ff01a2719bdb38d7ce6342995b187f25
Elise Melo <elisemelo@Elises-MacBook-Air.local>
2022-03-10 17:50:52 -0300

    feat: adding NavBar

---
+++ src/Components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../index.css';
+
+const NavBar = () => {
+    return (
+        <div className='NavBar'>
+                <h2 >doutormultas</h2>
+        </div>
+    )
+}
+
+export default NavBar;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ManufacturesController from './Screen/Manufacturers/ManufacturesController';
-import axios from 'axios'
+import axios from 'axios';
+import NavBar from './Components/NavBar'
 
 axios.defaults.baseURL = 'https://parallelum.com.br/fipe/api/v1/carros/';
 
 ReactDOM.render(
   <React.StrictMode>
+    <NavBar />
  <div>
    <ManufacturesController />
  </div>
