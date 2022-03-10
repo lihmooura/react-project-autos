7dd3954359792a794459b621282b37aba91e771c
Elise Melo <elisemelo@Elises-MacBook-Air.local>
2022-03-10 14:18:20 -0300

    feat: setting up MVC structure

---
+++ src/Screen/Vehicle/VehicleController.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import VehicleView from './VehicleView';
+
+const VehicleController = () => {
+    return (
+            <VehicleView />
+    )
+}
+
+export default VehicleController;
---
+++ src/Screen/Vehicle/VehicleView.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const VehicleView = () => {
+    return (
+        <div>
+            Hello
+        </div>
+    )
+}
+
+export default VehicleView;
